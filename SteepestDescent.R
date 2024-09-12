953d8368c4d8abbf0cf3b066ef7c57fbe7634995
Brigham Halverson <brighalvy@tamu.edu>
2024-09-12 09:40:51 -0500

    Forgot to commit work from class on 9/10.

--- SteepestDescent.R
+++ SteepestDescent.R
@@ -19,9 +19,9 @@ SteepestDescent <- function(f, fprime, x0, alpha, nIter){
     # and save the new function value
     # [ToDo] fill in
     # Steepest descent update
-
+    xvec[i + 1] <- xvec[i] - alpha * fprime(xvec[i])
     # Function value
-
+    fvec[i + 1] <- f(xvec[i + 1])
   }
   
   # Return the vector of x values, as well as the vector of function values across iterations, including the starting point (both length nIter + 1)
